906c04d87ff7df0f5607fb01ff7e8a8c7b44009d
tomatograce <geraldlyonga@gmail.com>
2021-09-11 18:03:39 +0100

    modified the mode of fetching from the api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+function Checkbox() {
+  const [checked, toggle] = useReducer(checked => !checked, false);
+
+  return (
+    <>
+    <input type="checkbox" value={checked} onChange={toggle} />
+    <h5>{checked ? "checked" : "not checked"}</h5>
+    </>
+  )
+}
+
+function GithubUser(props) {
+  const [data, setData] = useState(null);
+
+  useEffect( () => {
+    fetch(`https://api.github.com/users/${props.login}`)
+    .then(res => res.json())
+    .then(setData)
+    .catch(console.error);
+  }, [] );
+
+  if(data) {
+    return (
+      <>
+      <h1>{data.login}</h1>
+      <h3>Profile Pic</h3>
+      <img src={data.avatar_url} />
+      <h3>{data.company}</h3>
+      </>
+    );
+  }
+
+  return null;
+
+}
+
+function App() {
+
+  return (
+    <>
+    <Checkbox />
+    <GithubUser login="eveporcello" />
+    </>
+  )
+
+} 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>,
+  </>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
